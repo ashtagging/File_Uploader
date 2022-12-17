3ee6b8494e089df08d866b579dbd1ee7465b9020
Ashley Littlewood <ashley.littlewood22@gmail.com>
2022-12-17 22:19:56 +0000

    Added Main File Upload Functionality

---
+++ FileUploader/Services/FileUpload.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Hosting;
+
+namespace FileUploader.Services
+{
+    public interface IFileUpload
+    {
+        Task UploadFile(IBrowserFile file);
+    }
+    public class FileUpload : IFileUpload
+    {
+        private IWebHostEnvironment _webHostEnvironment;
+        private readonly ILogger<FileUpload> _logger;
+
+        public FileUpload(IWebHostEnvironment webHostEnvironment, ILogger<FileUpload> logger)
+        {
+            logger = _logger;
+            webHostEnvironment = _webHostEnvironment;
+        }
+        public async Task UploadFile(IBrowserFile file)
+        {
+            if(file != null)
+            {
+                try
+                {
+                    var uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, "uploads", file.Name);
+
+                    using(var stream = file.OpenReadStream())
+                    {
+                        var fileStream = File.Create(uploadPath);
+                        await stream.CopyToAsync(fileStream);
+                        fileStream.Close();
+                    }
+                }
+                catch(Exception e)
+                {
+                    _logger.LogError(e.ToString());
+                }
+            }
+        }
+    }
+}
