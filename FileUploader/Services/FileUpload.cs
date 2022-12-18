9ae31b139839f63c563b8ecc65abc326d5d1ee52
Ashley Littlewood <ashley.littlewood22@gmail.com>
2022-12-18 11:39:53 +0000

    Added Image Previewer

--- FileUploader/Services/FileUpload.cs
+++ FileUploader/Services/FileUpload.cs
@@ -6,6 +6,7 @@ namespace FileUploader.Services
     public interface IFileUpload
     {
         Task UploadFile(IBrowserFile file);
+        Task<string> GeneratePreviewUrl(IBrowserFile file);
     }
     public class FileUpload : IFileUpload
     {
@@ -39,6 +40,22 @@ namespace FileUploader.Services
                     Console.WriteLine(e.Message);
                 }
             }
+        }
+
+        public async Task<string> GeneratePreviewUrl(IBrowserFile file)
+        {
+            if (!file.ContentType.Contains("image"))
+            {
+                if (file.ContentType.Contains("pdf"))
+                {
+                    return "images/PDF_icon.png";
+                }
+            }
+
+            var resizedImage = await file.RequestImageFileAsync(file.ContentType, 100, 100);
+            var buffer = new byte[resizedImage.Size];
+            await resizedImage.OpenReadStream().ReadAsync(buffer);
+            return $"data:{file.ContentType};base64,{Convert.ToBase64String(buffer)}";
         }
     }
 }
